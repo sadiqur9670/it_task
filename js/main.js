a497a5d2357dd5051009cf4dcf67de54ad706c2b
Sadiqur Rahman Rifat <srrifat9670@gmail.com>
2021-06-13 13:09:46 +0600

    update js for change

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@
     e.addEventListener('click',edit);
   })
    document.querySelector('.edit-popup-box p').addEventListener('click', close);
+   document.querySelector(".update").addEventListener('click', close);
 
    const editPoput = document.querySelector(".edit-popup");
     function edit(){
@@ -17,14 +18,32 @@
       editPoput.classList.add("popup-close");
       
   }
+})();
+
+(() =>{
+  //get select row
+  //display selected row data in text input
+  var table = document.querySelector("#table"),rIndex;
+  var tableEdit = document.querySelector("#table .edit"),rIndex;
+
+  for(var i=1; i<table.rows.length; i++){
+    table.rows[i].onclick = function(){
+      rIndex = this.rowIndex;
+      document.querySelector("#exampleInputName1").value = this.cells[0].innerHTML;
+      document.querySelector("#exampleInputEmail1").value = this.cells[1].innerHTML;
+      document.querySelector("#exampleInputPhone1").value = this.cells[2].innerHTML;
+      document.querySelector("#exampleInputRole1").value = this.cells[3].innerHTML;
+    }
+  }
+
+
+  document.querySelector(".update").addEventListener('click', updating);
+   function updating(){
+      table.rows[rIndex].cells[0].innerHTML = document.querySelector("#exampleInputName1").value;
+      table.rows[rIndex].cells[1].innerHTML = document.querySelector("#exampleInputEmail1").value;
+      table.rows[rIndex].cells[2].innerHTML = document.querySelector("#exampleInputPhone1").value;
+      table.rows[rIndex].cells[3].innerHTML = document.querySelector("#exampleInputRole1").value;
+
+}
+  
 })()
-
-// $(document).ready(function(){
-//     var editPopupo = document.querySelector("#edit").addEventListener('click',edit);
-
-
-//     function edit(){
-//        editPopupo.classList.add("popup-open");
-//       editPopupo.classList.remove("popup-close");
-//   }
-// })
